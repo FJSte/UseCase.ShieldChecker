f95dbf09038053760dd4a12b51fa96ff903448b1
Thomas Kurth <thomas@famkurth.ch>
2025-07-16 20:28:13 +0200

    Initial Release Executor

---
+++ src/Executor/ShieldChecker.Executor.Core/JobUpdate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShieldChecker.Executor.Core
+{
+    public class JobUpdate
+    {
+        public int? Status { get; set; }
+        public required string TestOutput {  get; set; }
+        public required string ExecutorOutput { get; set; }
+    }
+}
